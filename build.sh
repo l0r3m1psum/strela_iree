bc3b0158d3efe0d853aec2be9e4b36268c4c4fb2
Diego Bellani <bellanidiego@gmail.com>
2026-07-12 17:50:06 +0200

    Added IREE v3.11.0

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+cmake -G Ninja -B iree-build -S 3rdparty/iree \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DIREE_ENABLE_ASSERTIONS=ON \
+    -DIREE_ENABLE_SPLIT_DWARF=ON \
+    -DIREE_ENABLE_THIN_ARCHIVES=ON \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DIREE_ENABLE_LLD=ON
+
+cmake --build iree-build
