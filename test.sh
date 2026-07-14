33167f2fc5319878ebe1d36e3043f03c2ecd9f11
Diego Bellani <bellanidiego@gmail.com>
2026-07-14 16:35:59 +0200

    Added small test script

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]
+then
+	echo "Missing path to .tflite file" >&2
+	exit 1
+fi
+
+model_path="$1"
+tosa-converter-for-tflite "$model_path" --text \
+	| sed 's/DOUBLE_ROUND/SINGLE_ROUND/g; s/?x/1x/g' >ad01_int8.mlir
+
+iree-opt --mlir-elide-elementsattrs-if-larger=16 ad01_int8.mlir
+
+# --iree-preprocessing-convert-1x1-filter-conv2d-to-matmul
+# --mlir-print-ir-after=iree-vm-conversion \
+iree-compile \
+	--iree-plugin=example2 \
+	--iree-example2-fusion \
+	--iree-hal-target-backends=llvm-cpu \
+	--iree-llvmcpu-target-cpu=host \
+	ad01_int8.mlir -o ad01_int8.vmfb
+
+iree-run-module \
+	--module=build/libcustom_module.so \
+	--module=ad01_int8.vmfb \
+	--function=main \
+	--input="1x640xi8=0"
