c162485c9c303e6cdae80cc03f6705125a2a8008
Diego Bellani <bellanidiego@gmail.com>
2026-09-07 14:23:39 +0200

    Cleanedup command_buffer class

--- src/my_driver/command_buffer.c
+++ src/my_driver/command_buffer.c
@@ -57,71 +57,62 @@ iree_hal_strela_command_buffer_create(
   return status;
 }
 
-static iree_status_t
-iree_hal_strela_command_buffer_dispatch(
-  iree_hal_command_buffer_t *base_command_buffer,
-  iree_hal_executable_t *executable,
-  iree_hal_executable_function_t executable_function,
-  const iree_hal_dispatch_config_t dispatch_config,
-  iree_const_byte_span_t const_byte_span,
-  iree_hal_buffer_ref_list_t buffer_ref_list,
-  iree_hal_dispatch_flags_t dispatch_flags
-) {
-
-  [[maybe_unused]] iree_hal_strela_command_buffer_t *cmd = (iree_hal_strela_command_buffer_t*)base_command_buffer;
-
-  // Initialize the configuration for the accelerator.
-  [[maybe_unused]] strela_conf conf = {0};
-
-  // Extract bound buffer offsets from the executable descriptor sets and
-  // map them to inp0_offset, inp1_offset, out1_offset, etc.
-  // conf.inp0_offset = ...
-
-  // Push this `conf` struct into your command buffer's internal queue.
-  // Do NOT execute the hardware here!
-  // ...
-
-  return iree_ok_status();
-}
-
 static void
 iree_hal_strela_command_buffer_destroy(iree_hal_command_buffer_t *base_command_buffer) {
-  [[maybe_unused]] iree_hal_strela_command_buffer_t *cmd = (iree_hal_strela_command_buffer_t *)base_command_buffer;
+  iree_hal_strela_command_buffer_t *command_buffer = iree_hal_strela_command_buffer_cast(base_command_buffer);
+  iree_allocator_t host_allocator = command_buffer->host_allocator;
 
-  // NOTE: Assuming you add `iree_allocator_t host_allocator` to iree_hal_strela_command_buffer_t
-  // iree_allocator_free(cmd->host_allocator, cmd);
+  iree_allocator_free(host_allocator, command_buffer);
 }
 
 static iree_status_t
-iree_hal_strela_command_buffer_begin(iree_hal_command_buffer_t *command_buffer) {
+iree_hal_strela_command_buffer_begin(iree_hal_command_buffer_t *base_command_buffer) {
   printf("%s\n", __func__);
+
+  iree_hal_strela_command_buffer_t *command_buffer = iree_hal_strela_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_ok_status();
 }
 
 static iree_status_t
-iree_hal_strela_command_buffer_end(iree_hal_command_buffer_t *command_buffer) {
+iree_hal_strela_command_buffer_end(iree_hal_command_buffer_t *base_command_buffer) {
   printf("%s\n", __func__);
+
+  iree_hal_strela_command_buffer_t *command_buffer = iree_hal_strela_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_ok_status();
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_begin_debug_group(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_string_view_t label,
   iree_hal_label_color_t label_color,
   const iree_hal_label_location_t *location
 ) {
+  iree_hal_strela_command_buffer_t *command_buffer = iree_hal_strela_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
-iree_hal_strela_command_buffer_end_debug_group(iree_hal_command_buffer_t* command_buffer) {
+iree_hal_strela_command_buffer_end_debug_group(iree_hal_command_buffer_t* base_command_buffer) {
+  iree_hal_strela_command_buffer_t *command_buffer = iree_hal_strela_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_execution_barrier(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_execution_stage_t source_stage_mask,
   iree_hal_execution_stage_t target_stage_mask,
   iree_hal_execution_barrier_flags_t flags,
@@ -131,30 +122,42 @@ iree_hal_strela_command_buffer_execution_barrier(
   const iree_hal_buffer_barrier_t *buffer_barriers
 ) {
   printf("%s\n", __func__);
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_ok_status();
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_signal_event(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_event_t *event,
   iree_hal_execution_stage_t source_stage_mask
 ) {
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_reset_event(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_event_t *event,
   iree_hal_execution_stage_t source_stage_mask
 ) {
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_wait_events(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_host_size_t event_count,
   const iree_hal_event_t **events,
   iree_hal_execution_stage_t source_stage_mask,
@@ -164,34 +167,46 @@ iree_hal_strela_command_buffer_wait_events(
   iree_host_size_t buffer_barrier_count,
   const iree_hal_buffer_barrier_t *buffer_barriers
 ) {
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_advise_buffer(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_buffer_ref_t buffer_ref,
   iree_hal_memory_advise_flags_t flags,
   uint64_t arg0,
   uint64_t arg1
 ) {
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_fill_buffer(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_buffer_ref_t target_ref,
   const void *pattern,
   iree_host_size_t pattern_length,
   iree_hal_fill_flags_t flags
 ) {
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_update_buffer(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   const void *source_buffer,
   iree_host_size_t source_offset,
   iree_hal_buffer_ref_t target_ref,
@@ -202,18 +217,22 @@ iree_hal_strela_command_buffer_update_buffer(
 
 static iree_status_t
 iree_hal_strela_command_buffer_copy_buffer(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_buffer_ref_t source_ref,
   iree_hal_buffer_ref_t target_ref,
   iree_hal_copy_flags_t flags
 ) {
   printf("%s\n", __func__);
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_ok_status();
 }
 
 static iree_status_t
 iree_hal_strela_command_buffer_collective(
-  iree_hal_command_buffer_t *command_buffer,
+  iree_hal_command_buffer_t *base_command_buffer,
   iree_hal_channel_t *channel,
   iree_hal_collective_op_t op,
   uint32_t param,
@@ -221,7 +240,29 @@ iree_hal_strela_command_buffer_collective(
   iree_hal_buffer_ref_t recv_ref,
   iree_device_size_t element_count
 ) {
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED, __func__);
+}
+
+static iree_status_t
+iree_hal_strela_command_buffer_dispatch(
+  iree_hal_command_buffer_t *base_command_buffer,
+  iree_hal_executable_t *executable,
+  iree_hal_executable_function_t executable_function,
+  const iree_hal_dispatch_config_t dispatch_config,
+  iree_const_byte_span_t const_byte_span,
+  iree_hal_buffer_ref_list_t buffer_ref_list,
+  iree_hal_dispatch_flags_t dispatch_flags
+) {
+  printf("%s\n", __func__);
+  iree_hal_null_command_buffer_t *command_buffer = iree_hal_null_command_buffer_cast(base_command_buffer);
+
+  (void)command_buffer;
+
+  return iree_ok_status();
 }
 
 static const iree_hal_command_buffer_vtable_t
